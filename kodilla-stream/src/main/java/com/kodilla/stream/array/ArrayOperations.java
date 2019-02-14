1fb1efad06b8446acde40c98f09ebbf4526432b9
piotr-michon <piotr.michon01@gmail.com>
2019-02-14 23:27:41 +0100

    7.5 Commit

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,4 +1,13 @@
 package com.kodilla.stream.array;
 
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+        IntStream.range(numbers[0], numbers.length+1)
+                .forEach(System.out::println);
+
+        return IntStream.range(numbers[0], numbers.length + 1)
+                .average().getAsDouble();
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,10 +1,17 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
-
+        //Given
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                  11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        //When
+        ArrayOperations.getAverage(numbers);
+        //Then
+        Assert.assertEquals(10.5, ArrayOperations.getAverage(numbers), 0);
     }
 }
