15e1b93ec388ba5a3d1f1df5e464ca3660a966e9
piotr-michon <piotr.michon01@gmail.com>
2019-04-11 22:01:08 +0200

    14.3 Commit

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,27 +1,41 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
-    @Bean
+    @Autowired
+    @Qualifier("toDoList")
+    TaskList toDoList;
+
+    @Autowired
+    @Qualifier("inProgressList")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("doneList")
+    TaskList doneList;
+
+    @Bean(name = "toDoList")
     public TaskList createToDoList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "inProgressList")
     public TaskList createInProgressList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "doneList")
     public TaskList createDoneList() {
         return new TaskList();
     }
 
     @Bean
     public Board createBoard() {
-        return new Board(createToDoList(), createInProgressList(), createDoneList());
+        return new Board(toDoList, inProgressList, doneList);
     }
 }
