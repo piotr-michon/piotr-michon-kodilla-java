e81302d5133859fc48f8e9b6c5bfa13d8629e89f
piotr-michon <piotr.michon01@gmail.com>
2019-04-28 18:53:35 +0200

    17.2 Commit

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.hibernate.task.dao;
+
+import com.kodilla.hibernate.task.Task;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskDaoTestSuite {
+    @Autowired
+    private TaskDao taskDao;
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    public void testTaskDaoSave() {
+        //Given
+        Task task = new Task(DESCRIPTION, 7);
+
+        //When
+        taskDao.save(task);
+
+        //Then
+        int id = task.getId();
+        Optional<Task> readTask = taskDao.findById(id);
+        Assert.assertTrue(readTask.isPresent());
+
+        //CleanUp
+        taskDao.deleteById(id);
+    }
+}
